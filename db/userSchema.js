5d4bdd9de2a1aca76ec42c756b5f640aa9ff4ca7
Khang <nnbaokhang@gmail.com>
2020-07-29 15:03:09 -0600

    add object to user

--- db/userSchema.js
+++ db/userSchema.js
@@ -3,7 +3,8 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
     username:{type:String, required: true, unique: true},
-    password: {type:String,required: true}
+    password: {type:String,required: true},
+    score: Object
 });
 
 var User = mongoose.model('user', userSchema);
